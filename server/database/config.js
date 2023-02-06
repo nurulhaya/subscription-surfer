df64b080f31288cb1d117f35602c511ab08a158c
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-02-05 19:54:13 -0500

    Import files

---
+++ server/database/config.js
@@ -0,0 +1,24 @@
+export default {
+  development: {
+    username: 'sql9596071',
+    password: 'jhiGI9PYyI',
+    database: 'sql9596071',
+    host: 'sql9.freesqldatabase.com',
+    dialect: 'mysql'
+  }
+  ,
+  test: {
+    username: 'sql9596071',
+    password: 'jhiGI9PYyI',
+    database: 'sql9596071',
+    host: 'sql9.freesqldatabase.com',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'sql9596071',
+    password: 'jhiGI9PYyI',
+    database: 'sql9596071',
+    host: 'sql9.freesqldatabase.com',
+    dialect: 'mysql'
+  }
+};
---
+++ server/models/index.js
@@ -0,0 +1,15 @@
+import Availability from './Availability.js';
+import Genres from './Genres.js';
+import Images from './Images.js';
+import Languages from './Languages.js';
+import Movies from './Movies.js';
+import Ratings from './Ratings.js';
+
+export default {
+  Availability,
+  Genres,
+  Images,
+  Languages,
+  Movies,
+  Ratings
+};
