60c054621a6a035981c976e944a34be3e259d7b3
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-03-13 14:57:37 -0400

    update test & prod db config

--- server/database/config.js
+++ server/database/config.js
@@ -8,7 +8,17 @@ export default {
   }
   ,
   test: {
+    username: 'sql9605372',
+    password: '2m3x1xjnTr',
+    database: 'sql9605372',
+    host: 'sql9.freesqldatabase.com',
+    dialect: 'mysql'
   },
   production: {
+    username: 'sql9605372',
+    password: '2m3x1xjnTr',
+    database: 'sql9605372',
+    host: 'sql9.freesqldatabase.com',
+    dialect: 'mysql'
   }
 };
