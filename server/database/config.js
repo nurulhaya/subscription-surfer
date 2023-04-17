58843ff2d46ace712c314457dbdb7f1887fce903
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2023-04-17 11:26:49 -0400

    Update db config

--- server/database/config.js
+++ server/database/config.js
@@ -1,24 +1,27 @@
 export default {
   development: {
-    username: 'sql9605372',
-    password: '2m3x1xjnTr',
-    database: 'sql9605372',
-    host: 'sql9.freesqldatabase.com',
+    username: 'avnadmin',
+    password: 'AVNS_wbNozRCRePIYO0Z4CO8',
+    database: 'streamingplatforms',
+    host: 'subscription-surfer-nhaya.aivencloud.com',
+    port: 16842,
     dialect: 'mysql'
   }
   ,
   test: {
-    username: 'sql9605372',
-    password: '2m3x1xjnTr',
-    database: 'sql9605372',
-    host: 'sql9.freesqldatabase.com',
+    username: 'avnadmin',
+    password: 'AVNS_wbNozRCRePIYO0Z4CO8',
+    database: 'streamingplatforms',
+    host: 'subscription-surfer-nhaya.aivencloud.com',
+    port: 16842,
     dialect: 'mysql'
   },
   production: {
-    username: 'sql9605372',
-    password: '2m3x1xjnTr',
-    database: 'sql9605372',
-    host: 'sql9.freesqldatabase.com',
+    username: 'avnadmin',
+    password: 'AVNS_wbNozRCRePIYO0Z4CO8',
+    database: 'streamingplatforms',
+    host: 'subscription-surfer-nhaya.aivencloud.com',
+    port: 16842,
     dialect: 'mysql'
   }
 };
